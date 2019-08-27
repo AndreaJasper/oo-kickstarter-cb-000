392b4d6decde50475f276e378cffcc11ab0c3216
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-08-27 15:06:52 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,8 +1,12 @@
 class Project
-  attr_reader :backer, :name
+  attr_reader :backers, :title
 
   def initialize(name)
     @name = name
-    @baker = []
+    @backers = []
   end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.add_project(self) unless backer.projects.include?(self)
 end
